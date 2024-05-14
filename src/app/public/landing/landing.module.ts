fcabd5c2cec026a3bb5e4b6e274cdf29ae973a47
Chaitali Dhamande <chaitali.dhamande@infobeans.com>
2024-05-14 15:10:04 +0530

    login button color changed

--- src/app/public/landing/landing.module.ts
+++ src/app/public/landing/landing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { RouterModule } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -10,7 +10,9 @@ import { RouterModule } from '@angular/router';
     LandingComponent
   ],
   imports: [
-    CommonModule,RouterModule
+    CommonModule,
+    RouterModule,
+    MatButtonModule
   ], 
   exports: [
     LandingComponent
